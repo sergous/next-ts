a1659a0f0c17d55413117c7a22927f9669425e83
Sergei Smirnov <sergous@gmail.com>
2018-12-06 00:13:35 -0800

    Add contacts page

---
+++ src/pages/contacts.tsx
@@ -0,0 +1,1 @@
+export default () => <p>Contacts page</p>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,17 @@ import Link from "next/link"
 export default () => (
   <div>
     Hello Next!
-    <Link href="/about">
-      <a>About</a>
-    </Link>
+    <ul>
+      <li>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/contacts">
+          <a>Contacts</a>
+        </Link>
+      </li>
+    </ul>
   </div>
 )
